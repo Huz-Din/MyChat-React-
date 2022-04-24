84dee67ab346ccb046df9c1218bf9ea500deb042
Huz-Din <dhuziahmetov@gmail.com>
2022-04-24 11:26:54 +0500

    task of lesson 7

--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -4,10 +4,11 @@ import TextField from "@mui/material/TextField";
 import Fab from "@mui/material/Fab";
 import LocalAirportIcon from "@mui/icons-material/LocalAirport";
 import Box from "@mui/material/Box";
-import AUTHOR from "./constants";
+// import AUTHOR from "./constants";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import addMessage from "./store/messages/actions";
+import addMessageWithThunk from "./store/messages/actions";
+// import { addMessageWithSaga } from "./store/messages/actions";
 
 const ControlPanel = () => {
   let { chatId } = useParams();
@@ -15,8 +16,8 @@ const ControlPanel = () => {
   const inputRef = useRef(null);
   const dispatch = useDispatch();
   const author = useSelector((state) => state.profile.name);
-  const allMessages = useSelector((state) => state.messages.messageList);
-  const messages = allMessages[chatId] || [];
+  // const allMessages = useSelector((state) => state.messages.messageList);
+  // const messages = allMessages[chatId] || [];
 
   const updateMessage = (event) => {
     setValue(event.target.value);
@@ -26,7 +27,8 @@ const ControlPanel = () => {
     e.preventDefault();
     if (value !== "") {
       const newMessage = { text: value, author };
-      dispatch(addMessage(chatId, newMessage));
+      dispatch(addMessageWithThunk(chatId, newMessage));
+      // dispatch(addMessageWithSaga(chatId, newMessage));
       setValue("");
       inputRef.current?.focus();
     }
@@ -36,23 +38,24 @@ const ControlPanel = () => {
     inputRef.current?.focus();
   }, []);
 
-  useEffect(() => {
-    let timerID;
-    if (
-      messages?.length > 0 &&
-      messages[messages.length - 1].author !== AUTHOR.bot
-    ) {
-      const newMessage = { text: "Hi! How are you?", author: AUTHOR.bot };
-      timerID = setTimeout(() => {
-        dispatch(addMessage(chatId, newMessage));
-      }, 1500);
-    }
-    return () => {
-      if (timerID) {
-        clearTimeout(timerID);
-      }
-    };
-  }, [messages, chatId]);
+  // deactived bot
+  // useEffect(() => {
+  //   let timerID;
+  //   if (
+  //     messages?.length > 0 &&
+  //     messages[messages.length - 1].author !== AUTHOR.bot
+  //   ) {
+  //     const newMessage = { text: "Hi! How are you?", author: AUTHOR.bot };
+  //     timerID = setTimeout(() => {
+  //       dispatch(addMessage(chatId, newMessage));
+  //     }, 1500);
+  //   }
+  //   return () => {
+  //     if (timerID) {
+  //       clearTimeout(timerID);
+  //     }
+  //   };
+  // }, [messages, chatId]);
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import "./index.css";
 import styles from "./index.module.css";
 import App from "./App";
 import { orange } from "@mui/material/colors";
-import { createTheme, ThemeProvider } from "@mui/material";
+import { CircularProgress, createTheme, ThemeProvider } from "@mui/material";
 import { Provider } from "react-redux";
-import store from "./store/index";
+import { store } from "./store/index";
+import persistor from "./store/index";
+import { PersistGate } from "redux-persist/integration/react";
 
 //тема проекта
 const theme = createTheme({
@@ -21,11 +23,13 @@ const theme = createTheme({
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <div className={styles.container}>
-          <App />
-        </div>
-      </ThemeProvider>
+      <PersistGate persistor={persistor} loadings={<CircularProgress />}>
+        <ThemeProvider theme={theme}>
+          <div className={styles.container}>
+            <App />
+          </div>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
